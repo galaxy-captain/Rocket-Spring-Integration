bbb31ed553dd7888a2681813d05b908fa3befeaf
Sander <goskkyer@gmail.com>
2019-06-19 10:50:40 +0800

    :sparkles: 增加@RocketConfiguration类级别配置

--- spring-rocketmq-integration/src/main/java/me/galaxy/rocketmq/annotation/RocketConfiguration.java
+++ spring-rocketmq-integration/src/main/java/me/galaxy/rocketmq/annotation/RocketConfiguration.java
@@ -1,9 +1,18 @@
 package me.galaxy.rocketmq.annotation;
 
+import me.galaxy.rocketmq.exception.NoException;
+
 /**
  * @Description
- * @Author duanxiaolei@bytedance.com
+ * @Author galaxy-captain
  * @Date 2019-06-19 09:55
  **/
 public @interface RocketConfiguration {
+
+    String nameServer() default "";
+
+    int delayTimeLevel() default Integer.MIN_VALUE;
+
+    Class<? extends Throwable>[] ignoredExceptions() default NoException.class;
+
 }
